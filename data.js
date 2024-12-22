b32c56410b6fca9fa43923db765ba7c5e01db71c
Omama Mahmood <98207789+runamoto@users.noreply.github.com>
2024-12-22 22:31:53 +0400

    aaa

--- data.js
+++ data.js
@@ -1,16 +1,14 @@
 export let people = [
-	{
-		name: "omama mahmood",
-		images: ["./images/omamers_1.png", "./images/omamers_2.png"],
-		description:
-			`so if someone wants to say something about their work or whatever,
-			they can add it here, and if they wanna add <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">links</a>`
-	},
-	{
-		name: "aaryan pashine",
-		images: ["./images/aaryan_1.png", "./images/aaryan_2.png"],
-		description:
-			`this guys is not in thesis, so you can take him out of here...
-			but this is another <a href="https://www.are.na/aaryan-pashine">link example</a>`
-	}
-]
+  {
+    name: "Alexandre Davilmar-Smith ",
+    images: ["./images/alex.png", "./images/alex2.png"],
+    description: `so if someone wants to say something about their work or whatever,
+			they can add it here, and if they wanna add <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">links</a>`,
+  },
+  {
+    name: "Aasiya Zahra",
+    images: ["./images/aasiya1.png", "./images/aasiya2.png"],
+    description: `this guys is not in thesis, so you can take him out of here...
+			but this is another <a href="https://www.are.na/aaryan-pashine">link example</a>`,
+  },
+];
