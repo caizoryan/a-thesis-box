617d59e3bf97c861da9a4ea584995bb319017745
Aaryan Pashine <pashineaaryan@gmail.com>
2024-12-22 13:36:26 -0500

    fixed images

--- data.js
+++ data.js
@@ -1,14 +1,14 @@
 export let people = [
-  {
-    name: "Alexandre Davilmar-Smith ",
-    images: ["./images/alex.png", "./images/alex2.png"],
-    description: `so if someone wants to say something about their work or whatever,
+	{
+		name: "Alexandre Davilmar-Smith ",
+		images: ["./images/alex.png", "./images/alex2.png"],
+		description: `so if someone wants to say something about their work or whatever,
 			they can add it here, and if they wanna add <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">links</a>`,
-  },
-  {
-    name: "Aasiya Zahra",
-    images: ["./images/aasiya1.png", "./images/aasiya2.png"],
-    description: `this guys is not in thesis, so you can take him out of here...
+	},
+	{
+		name: "Aasiya Zahra",
+		images: ["./images/aasiya1.png", "./images/aasiya2.png", "./images/aasiya3.png"],
+		description: `this guys is not in thesis, so you can take him out of here...
 			but this is another <a href="https://www.are.na/aaryan-pashine">link example</a>`,
-  },
+	},
 ];
