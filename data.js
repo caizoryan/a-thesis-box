0c09c9a896adcab0c4d3a295ea8a814ef795ab77
Omama Mahmood <98207789+runamoto@users.noreply.github.com>
2024-12-24 20:48:14 +0400

    help me fix the box shadow pls

--- data.js
+++ data.js
@@ -1,46 +1,132 @@
 export let people = [
-	{
-		name: "Alexandre Davilmar-Smith ",
-		images: ["./images/alex.png", "./images/alex2.png"],
-	},
-	{
-		name: "Aasiya Zahra",
-		images: [
-			"./images/aasiya1.png",
-			"./images/aasiya2.png",
-			"./images/aasiya3.png",
-		],
-	},
-	{
+  {
+    name: "Alexandre Davilmar-Smith ",
+    images: ["./images/alex.png", "./images/alex2.png"],
+  },
+  {
+    name: "Aasiya Zahra",
+    images: [
+      "./images/aasiya1.png",
+      "./images/aasiya2.png",
+      "./images/aasiya3.png",
+    ],
+  },
+  {
+    name: "Alishba Fahim ",
+    images: ["./images/alishba.png"],
+  },
+  {
+    name: "Amir Nevis",
+    images: ["./images/amir1.png", "./images/amir2.png", "./images/amir3.png"],
+  },
+  { name: "Amna Dada", images: ["./images/amna1.png", "./images/amna2.png"] },
+  {
+    name: "Dana Keselman",
+    images: ["./images/dana1.png", "./images/dana2.png", "./images/dana3.png"],
+  },
+  { name: "Emiliya Volchenko ", images: ["./images/emiliya1.png"] },
+  {
+    name: "Esther Calixto Sales",
+    images: [
+      "./images/esther1.png",
+      "./images/esther2.png",
+      "./images/esther3.png",
+    ],
+  },
+  {
+    name: "Francine Guilalas",
+    images: ["./images/francine1.png", "./images/francine2.png"],
+  },
+  { name: "Haeun Jang", images: ["./images/haeun.png"] },
+  {
+    name: "Kirthiga Thevakumar",
+    images: ["./images/kithi1.png", "./images/kithi2.png"],
+  },
+  {
+    name: "Lindsay Quintal",
+    images: [
+      "./images/lindsay1.png",
+      "./images/lindsay2.png",
+      "./images/lindsay3.png",
+    ],
+  },
+  {
+    name: "N'dasa Todd",
+    images: [
+      "./images/ndasa1.png",
+      "./images/ndasa2.png",
+      "./images/ndasa3.png",
+    ],
+  },
+  { name: "Nikhiel Tejani", images: ["./images/nikhel1.png"] },
+  {
+    name: "Noura Khaki",
+    images: ["./images/noura.png", "./images/noura2.png"],
+  },
+  { name: "Omama", images: ["./images/omama1.png"] },
+  {
+    name: "Pegah Resalat Panah",
+    images: [
+      "./images/pegah1.png",
+      "./images/pegah2.png",
+      "./images/pegah3.png",
+    ],
+  },
+  {
+    name: "Sadie Lambert",
+    images: [
+      "./images/sadie1.png",
+      "./images/sadie2.png",
+      "./images/sadie3.png",
+      "./images/sadie4.png",
+    ],
+  },
+  { name: "Shovemiya Packiyanathan", images: ["./images/sammi-shovemiya.png"] },
+  {
+    name: "Sammi Lam",
+    images: [
+      "./images/sammi1.png",
+      "./images/sammi2.png",
+      "./images/sammi5.png",
+      "./images/sammi3.png",
+      "./images/sammi4.png",
+    ],
+  },
+  {
+    name: "Sarita Williams",
+    images: ["./images/sarita.png", "./images/sarita2.png"],
+  },
+  {
+    name: "Simar Kaur",
+    images: [
+      "./images/simar1.png",
+      "./images/simar2.png",
+      "./images/simar3.png",
+    ],
+  },
+  { name: "Simran Khanna", images: ["./images/simran1.png"] },
+  {
+    name: "Yuxin Wenpan",
+    images: ["./images/yuxin1.png", "./images/yuxin2.png"],
+  },
+  {
+    name: "Zaid Irfan",
+    images: [
+      "./images/zaid-poster.png",
+      "./images/zaid.png",
+      "./images/zaid1.png",
+      "./images/zaid2.png",
+      "./images/zaid3.png",
+      "./images/zaid4.png",
+    ],
+  },
+  {
+    name: "Exhibition",
+    images: ["./images/exhibition1.png", "./images/exhibition2.png"],
+  },
 
-		name: "Alishba Fahim ",
-		images: ["./images/alishba.png"],
-	},
-	{
-		name: "Amir",
-		images: ["./images/amir1.png", "./images/amir2.png", "./images/amir3.png"],
-	},
-	{ name: "Amna", images: ["./images/amna1.png", "./images/amna2.png"] },
-	{ name: "Dana", images: ["./images/dana1.png", "./images/dana2.png", "./images/dana3.png"] },
-	{ name: "Emiliya", images: ["./images/emiliya1.png"] },
-	{ name: "Esther", images: ["./images/esther1.png", "./images/esther2.png", "./images/esther3.png"] },
-	{ name: "Exhibition", images: ["./images/exhibition1.png", "./images/exhibition2.png"] },
-	{ name: "Francine", images: ["./images/francine1.png", "./images/francine2.png"] },
-	{ name: "Haeun", images: ["./images/haeun.png"] },
-	{ name: "Kithi", images: ["./images/kithi1.png", "./images/kithi2.png"] },
-	{ name: "Lindsay", images: ["./images/lindsay1.png", "./images/lindsay2.png", "./images/lindsay3.png"] },
-	{ name: "Ndasa", images: ["./images/ndasa1.png", "./images/ndasa2.png", "./images/ndasa3.png"] },
-	{ name: "Nikhel", images: ["./images/nikhel1.png"] },
-	{ name: "Noura", images: ["./images/noura.png", "./images/noura2.png"] },
-	{ name: "Omama", images: ["./images/omama1.png"] },
-	{ name: "Pegah", images: ["./images/pegah1.png", "./images/pegah2.png", "./images/pegah3.png"] },
-	{ name: "Sadie", images: ["./images/sadie1.png", "./images/sadie2.png", "./images/sadie3.png", "./images/sadie4.png"] },
-	{ name: "Sammi Shovemiya", images: ["./images/sammi-shovemiya.png"] },
-	{ name: "Sammi", images: ["./images/sammi1.png", "./images/sammi2.png", "./images/sammi5.png", "./images/sammi3.png", "./images/sammi4.png"] },
-	{ name: "Sarita", images: ["./images/sarita.png", "./images/sarita2.png"] },
-	{ name: "Simar", images: ["./images/simar1.png", "./images/simar2.png", "./images/simar3.png"] },
-	{ name: "Simran", images: ["./images/simran1.png"] },
-	{ name: "Yuxin", images: ["./images/yuxin1.png", "./images/yuxin2.png"] },
-	{ name: "Zaid", images: ["./images/zaid-poster.png", "./images/zaid.png", "./images/zaid1.png", "./images/zaid2.png", "./images/zaid3.png", "./images/zaid4.png"] },
+  {
+    name: "a (virtual) exhibition",
+    images: ["./images/invite.png"],
+  },
 ];
-
